8692ebae509536a07087399fcdff3b08662d842c
Frank Wolf <fwolf@posteo.de>
2020-05-10 19:23:24 +0200

    fix: basic auth use, requirements.txt and local docker-compose.yml

--- app.py
+++ app.py
@@ -24,6 +24,8 @@ app = Flask(__name__)
 
 app.config['BASIC_AUTH_USERNAME'] = 'admin'
 app.config['BASIC_AUTH_PASSWORD'] = 'udacity'
+
+basic_auth = BasicAuth(app)
 app.config['BASIC_AUTH_FORCE'] = True
 
 moment = Moment(app)
