27afead91079361fb32ac326da9e3c4db2b9f2c1
Frank Wolf <fwolf@posteo.de>
2020-05-10 16:08:58 +0200

    try: use before_script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
 echo setting up env $DYNAMIC_ENV_VAR
-apk update && apk add sudo
-apk add --update py-pip
-pip install -U setuptools
-apk add python-dev libffi-dev openssl-dev gcc libc-dev make
-pip install --upgrade pip
-pip install docker-compose
+docker-compose down
 docker image prune -f
 docker-compose -f docker-compose.yml build --no-cache
 docker-compose -f docker-compose.yml up -d
