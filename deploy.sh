265cd775014517da4411eb15bc9fa82955931f5e
Frank Wolf <fwolf@posteo.de>
2020-05-10 15:13:35 +0200

    try: use apk

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 echo setting up env $DYNAMIC_ENV_VAR
-apt-get update && apt-get -y install sudo
-sudo apt-get update && sudo apt-get install bash
-sudo apt-get install -y python-pip
+apk update && apk add sudo
+sudo apk update && sudo apk bash
+sudo apk add --update py-pip
 sudo pip install docker-compose
 sudo docker image prune -f
 sudo docker-compose -f docker-compose.yml build --no-cache
