133330669712e5dad4473b8f8e1845a62468f97e
Frank Wolf <fwolf@posteo.de>
2020-05-10 16:51:13 +0200

    try: env vars for db connection

--- start.sh
+++ start.sh
@@ -1,1 +1,4 @@
-GUNICORN_CMD_ARGS="--bind=0.0.0.0:7001 --workers=1 --log-file=- --reload" gunicorn app:app -e PYTHONUNBUFFERED=True -e DOCKER=True
+echo "BEGIN - docker-var:"
+echo $DOCKER
+echo "END - docker-var."
+GUNICORN_CMD_ARGS="--bind=0.0.0.0:7001 --workers=1 --log-file=- --reload" gunicorn app:app -e PYTHONUNBUFFERED='True' -e DOCKER='True'
