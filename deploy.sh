2874541a011986a39f9c764a7437f92bec787087
Frank Wolf <fwolf@posteo.de>
2020-05-10 15:20:33 +0200

    try: installing more dependencies

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,11 @@
 
 echo setting up env $DYNAMIC_ENV_VAR
 apk update && apk add sudo
-sudo apk update && sudo apk bash
-sudo apk add --update py-pip
-sudo pip install docker-compose
-sudo docker image prune -f
-sudo docker-compose -f docker-compose.yml build --no-cache
-sudo docker-compose -f docker-compose.yml up -d
+apk add --update py-pip
+pip install -U setuptools
+apk add python-dev libffi-dev openssl-dev gcc libc-dev make
+pip install --upgrade pip
+pip install docker-compose
+docker image prune -f
+docker-compose -f docker-compose.yml build --no-cache
+docker-compose -f docker-compose.yml up -d
